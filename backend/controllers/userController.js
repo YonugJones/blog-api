ab27a15fd8072e06a5fa7258d65567a2115837f9
YonugJones <peterckerfoot@gmail.com>
2024-12-07 17:00:31 -0600

    added softDelete function to userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,6 +11,10 @@ const getAllUsers = asyncHandler(async (req, res) => {
     },
   });
 
+  if (!users) {
+    throw new CustomError('Users not found', 404);
+  }
+
   return res.status(200).json(users);
 });
 
@@ -20,6 +24,7 @@ const getUser = asyncHandler(async (req, res) => {
     where: { id: userId },
     select: { id: true, username: true, email: true, posts: true, comments: true },
   });
+
   if (!user) {
     throw new CustomError('User not found', 404);
   }
@@ -27,7 +32,33 @@ const getUser = asyncHandler(async (req, res) => {
   return res.status(200).json(user)
 });
 
+const softDeleteUser = asyncHandler(async (req, res) => {
+  const userId = parseInt(req.params.id, 10);
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    select: { id: true, username: true, email: true },
+  });
+
+  if (!user) {
+    throw new CustomError('User not found', 404);
+  };
+
+  await prisma.$transaction([
+    prisma.user.update({ where: { id: userId }, data: { isDeleted: true } }),
+    prisma.post.updateMany({ where: { authorId: userId }, data: { isDeleted: true } }),
+    prisma.comment.updateMany({ where: { userId: userId }, data: { isDeleted: true } })
+  ]);
+
+  const updatedUser = await prisma.user.findUnique({
+    where: { id: userId },
+    select: { id: true, username: true, email: true, isDeleted: true }
+  })
+
+  return res.status(200).json(updatedUser);
+});
+
 module.exports = {
   getAllUsers,
-  getUser
-}
+  getUser,
+  softDeleteUser
+};
---
+++ backend/prisma/migrations/20241207222523_add_soft_delete_fields/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Comment" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
