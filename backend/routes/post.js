5d9f1d2787cd8e3c6114d22f74c21e46d2030c54
YonugJones <peterckerfoot@gmail.com>
2024-12-09 15:43:26 -0600

    created route for getPostById

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getAllPosts, createPost } = require('../controllers/postController');
+const { getAllPosts, getPostById, createPost } = require('../controllers/postController');
 const { authenticateToken } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/', getAllPosts);
+router.get('/:id', authenticateToken, getPostById);
 router.post('/create', authenticateToken, createPost);
 
 module.exports = router;
