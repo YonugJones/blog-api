953d6109f16431c53a5b494552e7283c5c08f018
YonugJones <peterckerfoot@gmail.com>
2024-12-26 17:35:05 -0600

    created Header component

---
+++ frontend/public-frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <h1>My Blog</h1>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
