bf3a2069ecb0bbdb97e510b64d58494d779487c0
YonugJones <peterckerfoot@gmail.com>
2024-12-27 10:19:38 -0600

    create About component

---
+++ frontend/public-frontend/src/components/About/About.jsx
@@ -0,0 +1,22 @@
+export default function About() {
+  return (
+    <div className='about'>
+      <h2>About this blog</h2>
+      <p>
+        Welcome to my blog! This project is part of my journey to mastering full-stack development. 
+        It serves as a platform to integrate a custom-built front end with a backend API I developed, 
+        offering hands-on experience in designing and implementing a complete web application.
+      </p>
+      <p>
+        Here, I share thoughts and insights related to my programming journey. 
+        While currently a solo endeavor, this project reflects my aspirations to grow as a developer 
+        and potentially collaborate with others in the future.
+      </p>
+      <p>
+        Thank you for visiting! I&apos;m always open to feedback or collaboration opportunities. 
+        Your support and insights are greatly appreciated as I continue refining my skills and exploring 
+        the possibilities of full-stack development.
+      </p>
+    </div>
+  )
+}
