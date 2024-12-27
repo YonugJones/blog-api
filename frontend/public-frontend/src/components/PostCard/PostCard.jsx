e13b33742256b27ba1960e65318e26a920e08090
YonugJones <peterckerfoot@gmail.com>
2024-12-26 20:00:35 -0600

    created PostCard component

---
+++ frontend/public-frontend/src/components/PostCard/PostCard.jsx
@@ -0,0 +1,9 @@
+export default function PostCard({ post, onClick }) {
+  return (
+    <div className='post-card' onClick={() => onClick(post.id)}>
+      <img src={post.imageUrl} alt={post.title} className='post-card-image' />
+      <h2 className='post-card-title'>{post.title}</h2>
+      <p className='post-card-exerpt'>{post.exercpt}</p>
+    </div>
+  )
+}
