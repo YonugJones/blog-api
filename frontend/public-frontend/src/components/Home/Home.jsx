27c398719e21762d701166eac5ae852eee2715a0
YonugJones <peterckerfoot@gmail.com>
2024-12-27 10:11:53 -0600

    created Home component

---
+++ frontend/public-frontend/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className='home'>
+      <h2>Welcome to my blog!</h2>
+      <p>Explore the latest posts and updates below. Click on a post to read more!</p>
+      <div className='post-list'>
+        <p>Posts will appear here</p>
+      </div>
+    </div>
+  )
+}
