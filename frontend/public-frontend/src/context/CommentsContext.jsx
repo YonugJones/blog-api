fb2d3b67c567f297c2b50628d3f7b278566039fa
YonugJones <peterckerfoot@gmail.com>
2025-01-01 16:19:56 -0600

    added unlikeComment

--- frontend/public-frontend/src/context/CommentsContext.jsx
+++ frontend/public-frontend/src/context/CommentsContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from 'react';
-import { likeComment as apiLikeComment } from '../api/api.js';
+import { likeComment as apiLikeComment, unlikeComment as apiUnlikeComment } from '../api/api.js';
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const CommentsContext = createContext();
@@ -13,19 +13,42 @@ export const CommentsProvider = ({ children }) => {
 
   const likeComment = async (postId, commentId) => {
     try {
-      const updatedComment = await apiLikeComment(postId, commentId);
-      setComments((prevComments) =>
-        prevComments.map((comment) =>
-          comment.id === commentId ? updatedComment : comment
-        )
-      );
+      const response = await apiLikeComment(postId, commentId);
+      if (response.success) {
+        const updatedComment = response.comment
+        setComments((prevComments) =>
+          prevComments.map((comment) =>
+            comment.id === commentId ? updatedComment : comment
+          )
+        );
+      } else {
+        alert(response.message);
+      }
     } catch (error) {
       console.error('Error liking comment:', error);
     }
   };
 
+  const unlikeComment = async (postId, commentId) => {
+    try {
+      const response = await apiUnlikeComment(postId, commentId);
+      if (response.success) {
+        const updatedComment = response.comment;
+        setComments((prevComments) =>
+          prevComments.map((comment) =>
+            comment.id === commentId ? updatedComment : comment
+          )
+        );
+      } else {
+        alert(response.message);
+      }
+    } catch (error) {
+      console.error('Error unliking comment:', error);
+    }
+  };
+
   return (
-    <CommentsContext.Provider value={{ comments, setComments, addComment, likeComment }}>
+    <CommentsContext.Provider value={{ comments, setComments, addComment, likeComment, unlikeComment }}>
       {children}
     </CommentsContext.Provider>
   );
