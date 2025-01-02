274c0711fd85f7df8de7056538129e355af9079c
YonugJones <peterckerfoot@gmail.com>
2025-01-01 18:55:13 -0600

    added useErrorHandling

--- frontend/public-frontend/src/context/CommentsContext.jsx
+++ frontend/public-frontend/src/context/CommentsContext.jsx
@@ -1,36 +1,40 @@
 import { createContext, useState } from 'react';
 import { likeComment as apiLikeComment, unlikeComment as apiUnlikeComment } from '../api/api.js';
+import useErrorHandling from '../hooks/useErrorHandling';
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const CommentsContext = createContext();
 
 export const CommentsProvider = ({ children }) => {
   const [comments, setComments] = useState([]);
+  const { error, handleError, clearError } = useErrorHandling();
 
   const addComment = (newComment) => {
     setComments((prevComments) => [...prevComments, newComment]);
-  }; 
+  };
 
   const likeComment = async (postId, commentId) => {
     try {
+      clearError();
       const response = await apiLikeComment(postId, commentId);
       if (response.success) {
-        const updatedComment = response.comment
+        const updatedComment = response.comment;
         setComments((prevComments) =>
           prevComments.map((comment) =>
             comment.id === commentId ? updatedComment : comment
           )
         );
       } else {
-        alert(response.message);
+        handleError({ response });
       }
     } catch (error) {
-      console.error('Error liking comment:', error);
+      handleError(error);
     }
   };
 
   const unlikeComment = async (postId, commentId) => {
     try {
+      clearError();
       const response = await apiUnlikeComment(postId, commentId);
       if (response.success) {
         const updatedComment = response.comment;
@@ -40,15 +44,15 @@ export const CommentsProvider = ({ children }) => {
           )
         );
       } else {
-        alert(response.message);
+        handleError({ response });
       }
     } catch (error) {
-      console.error('Error unliking comment:', error);
+      handleError(error);
     }
   };
 
   return (
-    <CommentsContext.Provider value={{ comments, setComments, addComment, likeComment, unlikeComment }}>
+    <CommentsContext.Provider value={{ comments, setComments, addComment, likeComment, unlikeComment, error }}>
       {children}
     </CommentsContext.Provider>
   );
