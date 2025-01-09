0fa8415b8a6c47c04382903a2c8f7c9d15f924f2
YonugJones <peterckerfoot@gmail.com>
2025-01-09 11:33:53 -0600

    created auth api call functions

---
+++ frontend/private-frontend/src/api/authAPI.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const API_BASE = 'http://localhost:3000';
+
+export const apiClient = axios.create({
+  baseURL: API_BASE
+});
+
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export const signup = (userData) => 
+  apiClient.post('/auth/signup', userData).then((res) => res.data);
+export const login = (credentials) => 
+  apiClient.post('/auth/login', credentials).then((res) => {
+    const { token, user } = res.data;
+    localStorage.setItem('token', token);
+    localStorage.setItem('userId', user.id);
+    return res.data
+  });
+export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('userId');
+  window.location.reload();
+};
