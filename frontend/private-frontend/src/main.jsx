2d9e8958484abd164724c118950f9ead12d04d2b
YonugJones <peterckerfoot@gmail.com>
2025-01-09 12:15:46 -0600

    wrap app with AuthProvider

--- frontend/private-frontend/src/main.jsx
+++ frontend/private-frontend/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import routes from './routes';
+import { AuthProvider } from './context/authContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={routes} />
+    <AuthProvider>
+      <RouterProvider router={routes} />
+    </AuthProvider>
   </StrictMode>,
 )
