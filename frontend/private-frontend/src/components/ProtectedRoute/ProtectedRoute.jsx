080d6742cca30aca10dccb3ef86724f61988c5b2
YonugJones <peterckerfoot@gmail.com>
2025-01-10 12:40:03 -0600

    added ProtectedRoute component

---
+++ frontend/private-frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,28 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../../context/authContext";
+import PropTypes from 'prop-types';
+
+const ProtectedRoute = ({ adminOnly = false, redirectPath = '/login' }) => {
+  const { isLoggedIn, isAdmin, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>
+  };
+
+  if (!isLoggedIn) {
+    return <Navigate to={redirectPath} replace/>
+  };
+
+  if (adminOnly && !isAdmin) {
+    return <Navigate to='/' replace/>
+  }
+
+  return <Outlet />
+}
+
+export default ProtectedRoute;
+
+ProtectedRoute.propTypes = {
+  adminOnly: PropTypes.bool,
+  redirectPath: PropTypes.string
+}
